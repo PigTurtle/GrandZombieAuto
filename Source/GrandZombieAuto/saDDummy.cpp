50a777f43e367724d791c49e0e6d71c695b38254
Song Hyesu <spdlqj183@gmail.com>
2022-06-14 18:33:25 +0900

    saDDummy file commit

---
+++ Source/GrandZombieAuto/saDDummy.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+// 이것은 소스트리 설명을 위해 더미로 제작되었다.
+
+#include "saDDummy.h"
+
+saDDummy::saDDummy()
+{
+}
+
+saDDummy::~saDDummy()
+{
+}
---
+++ Source/GrandZombieAuto/saDDummy.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+// 이것은 더미 소스파일의 헤더파일이다.
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GRANDZOMBIEAUTO_API saDDummy
+{
+public:
+	saDDummy();
+	~saDDummy();
+};
